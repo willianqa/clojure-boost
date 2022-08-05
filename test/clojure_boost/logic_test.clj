e0c61b87129de5dd1e899a20624f321a88e9dc34
Willian Nunes <willian.nunes@nubank.com.br>
2022-08-05 16:52:03 -0300

    adjust import

--- test/clojure_boost/logic_test.clj
+++ test/clojure_boost/logic_test.clj
@@ -3,6 +3,7 @@
   (:require [clojure.test :refer :all]
             [semana1.modelo-com-csv.csv :as semana1]
             [semana3.schema :as semana3]
+            [semana3.core :as core]
             [semana3.logic :as logic]
             [schema.core :as s]))
 
@@ -56,7 +57,7 @@
 
 (deftest schema-de-compras?-test
   (testing "valida o shcema de compras"
-    (is (= (semana3/nova-compra "2022-05-09" 100M "Amazon" "Casa" 1111222233334444)
+    (is (= (core/nova-compra "2022-05-09" 100M "Amazon" "Casa" 1111222233334444)
            {:data            "2022-05-09",
             :valor           100M,
             :estabelecimento "Amazon",
@@ -65,172 +66,172 @@
 
   (testing "Compra com data inválida"
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "03-08-2022" 100M "Amazon" "Casa" 1111222233334444))))
+                 (core/nova-compra "03-08-2022" 100M "Amazon" "Casa" 1111222233334444))))
 
   (testing "Compra com valor negativo"
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-03" -100M "Amazon" "Casa" 1111222233334444))))
+                 (core/nova-compra "2022-08-03" -100M "Amazon" "Casa" 1111222233334444))))
 
   (testing "Estabelecimento com String vazia"
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-03" 100M " " "Casa" 1111222233334444))))
+                 (core/nova-compra "2022-08-03" 100M " " "Casa" 1111222233334444))))
 
   (testing "Categoria Inválida"
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-03" 100M "Amazon" "PBKids" 1111222233334444))))
+                 (core/nova-compra "2022-08-03" 100M "Amazon" "PBKids" 1111222233334444))))
 
   (testing "Cartao com número Inválido"
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-03" 100M "Amazon" "Casa" 111122223333444499)))))
+                 (core/nova-compra "2022-08-03" 100M "Amazon" "Casa" 111122223333444499)))))
 
 ;--------------------------------------------------------------------------------
 
 (deftest outros-testes-data?
   (testing "testando limites da data"
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2 0 2 2-08-03" 100M "Amazon" "Casa" 1111222233334444)))
+                 (core/nova-compra "2 0 2 2-08-03" 100M "Amazon" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-0 8-03" 100M "Amazon" "Casa" 1111222233334444)))
+                 (core/nova-compra "2022-0 8-03" 100M "Amazon" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-0 3" 100M "Amazon" "Casa" 1111222233334444)))
+                 (core/nova-compra "2022-08-0 3" 100M "Amazon" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "mmmm-08-03" 100M "Amazon" "Casa" 1111222233334444)))
+                 (core/nova-compra "mmmm-08-03" 100M "Amazon" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-MM-03" 100M "Amazon" "Casa" 1111222233334444)))
+                 (core/nova-compra "2022-MM-03" 100M "Amazon" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-dd" 100M "Amazon" "Casa" 1111222233334444)))
+                 (core/nova-compra "2022-08-dd" 100M "Amazon" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra " " 100M "Amazon" "Casa" 1111222233334444)))
+                 (core/nova-compra " " 100M "Amazon" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "-08-03" 100M "Amazon" "Casa" 1111222233334444)))
+                 (core/nova-compra "-08-03" 100M "Amazon" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-03" 100M "Amazon" "Casa" 1111222233334444)))
+                 (core/nova-compra "2022-03" 100M "Amazon" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08" 100M "Amazon" "Casa" 1111222233334444)))
+                 (core/nova-compra "2022-08" 100M "Amazon" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022" 100M "Amazon" "Casa" 1111222233334444)))
+                 (core/nova-compra "2022" 100M "Amazon" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra 20220804 100M "Amazon" "Casa" 1111222233334444)))))
+                 (core/nova-compra 20220804 100M "Amazon" "Casa" 1111222233334444)))))
 
 (deftest otros-testes-valor?
   (testing "Testando limites do valor"
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 100 "Amazon" "Casa" 1111222233334444)))
+                 (core/nova-compra "2022-08-10" 100 "Amazon" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 100.0 "Amazon" "Casa" 1111222233334444)))
+                 (core/nova-compra "2022-08-10" 100.0 "Amazon" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 100.00 "Amazon" "Casa" 1111222233334444)))
+                 (core/nova-compra "2022-08-10" 100.00 "Amazon" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 100.50 "Amazon" "Casa" 1111222233334444)))
+                 (core/nova-compra "2022-08-10" 100.50 "Amazon" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" "100M" "Amazon" "Casa" 1111222233334444)))
+                 (core/nova-compra "2022-08-10" "100M" "Amazon" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ArityException
-                 (semana3/nova-compra "2022-08-10" "Amazon" "Casa" 1111222233334444)))
+                 (core/nova-compra "2022-08-10" "Amazon" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 0M "Amazon" "Casa" 1111222233334444)))
+                 (core/nova-compra "2022-08-10" 0M "Amazon" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 0 "Amazon" "Casa" 1111222233334444)))
+                 (core/nova-compra "2022-08-10" 0 "Amazon" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" -10M "Amazon" "Casa" 1111222233334444)))
+                 (core/nova-compra "2022-08-10" -10M "Amazon" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" -10 "Amazon" "Casa" 1111222233334444)))
+                 (core/nova-compra "2022-08-10" -10 "Amazon" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" -0.10 "Amazon" "Casa" 1111222233334444)))
+                 (core/nova-compra "2022-08-10" -0.10 "Amazon" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" -10.0 "Amazon" "Casa" 1111222233334444)))))
+                 (core/nova-compra "2022-08-10" -10.0 "Amazon" "Casa" 1111222233334444)))))
 
 (deftest outros-testes-estabelecimento?
   (testing "Validando limites do estabelecimento"
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 10M "A" "Casa" 1111222233334444)))
+                 (core/nova-compra "2022-08-10" 10M "A" "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 10M 2 "Casa" 1111222233334444)))
+                 (core/nova-compra "2022-08-10" 10M 2 "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 10M nil "Casa" 1111222233334444)))
+                 (core/nova-compra "2022-08-10" 10M nil "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 10M " " "Casa" 1111222233334444)))
+                 (core/nova-compra "2022-08-10" 10M " " "Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ArityException
-                 (semana3/nova-compra "2022-08-10" 10M "Casa" 1111222233334444)))))
+                 (core/nova-compra "2022-08-10" 10M "Casa" 1111222233334444)))))
 
 (deftest outros-testes-categoria?
   (testing "Validando limites da categoria"
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 10M "Alimentação" "CASA" 1111222233334444)))
+                 (core/nova-compra "2022-08-10" 10M "Alimentação" "CASA" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 10M "Alimentação" "CasA" 1111222233334444)))
+                 (core/nova-compra "2022-08-10" 10M "Alimentação" "CasA" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 10M "Alimentação" " Casa" 1111222233334444)))
+                 (core/nova-compra "2022-08-10" 10M "Alimentação" " Casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 10M "Alimentação" "Casa " 1111222233334444)))
+                 (core/nova-compra "2022-08-10" 10M "Alimentação" "Casa " 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 10M "Alimentação" "casa" 1111222233334444)))
+                 (core/nova-compra "2022-08-10" 10M "Alimentação" "casa" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 10M "Alimentação" "PBKids" 1111222233334444)))
+                 (core/nova-compra "2022-08-10" 10M "Alimentação" "PBKids" 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 10M "Alimentação" nil 1111222233334444)))
+                 (core/nova-compra "2022-08-10" 10M "Alimentação" nil 1111222233334444)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 10M "Alimentação" " " 1111222233334444)))
+                 (core/nova-compra "2022-08-10" 10M "Alimentação" " " 1111222233334444)))
 
     (is (thrown? clojure.lang.ArityException
-                 (semana3/nova-compra "2022-08-10" 10M "Alimentação" 1111222233334444)))))
+                 (core/nova-compra "2022-08-10" 10M "Alimentação" 1111222233334444)))))
 
 (deftest outros-testes-cartao?
   (testing "Validando limites do cartão"
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 10M "Alimentação" "Casa" 0)))
+                 (core/nova-compra "2022-08-10" 10M "Alimentação" "Casa" 0)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 10M "Alimentação" "Casa" 10000000000000000)))
+                 (core/nova-compra "2022-08-10" 10M "Alimentação" "Casa" 10000000000000000)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 10M "Alimentação" "Casa" -1)))
+                 (core/nova-compra "2022-08-10" 10M "Alimentação" "Casa" -1)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 10M "Alimentação" "Casa" "1000000000000000")))
+                 (core/nova-compra "2022-08-10" 10M "Alimentação" "Casa" "1000000000000000")))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 10M "Alimentação" "Casa" 1000000000000000M)))
+                 (core/nova-compra "2022-08-10" 10M "Alimentação" "Casa" 1000000000000000M)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 10M "Alimentação" "Casa" 0.1000000000000000)))
+                 (core/nova-compra "2022-08-10" 10M "Alimentação" "Casa" 0.1000000000000000)))
 
     (is (thrown? clojure.lang.ExceptionInfo
-                 (semana3/nova-compra "2022-08-10" 10M "Alimentação" "Casa" nil)))
+                 (core/nova-compra "2022-08-10" 10M "Alimentação" "Casa" nil)))
 
     (is (thrown? clojure.lang.ArityException
-                 (semana3/nova-compra "2022-08-10" 10M "Alimentação" "Casa" )))
+                 (core/nova-compra "2022-08-10" 10M "Alimentação" "Casa" )))
     ))
 
